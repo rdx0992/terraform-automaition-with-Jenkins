bbca69f72c1e87b71fe36feee6d35fc7b7c5026c
unknown <errahul0992@gmail.com>
2023-05-04 01:10:55 +0530

    lock remove

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mydev-tf-state-bucket-rahul"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dyanamo-db-table"
+    //dynamodb_table = "my-dyanamo-db-table"
   }
 }
