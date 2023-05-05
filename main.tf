8addd162daab9873e9d4074b9e7a1506f9532d32
unknown <errahul0992@gmail.com>
2023-05-05 20:59:45 +0530

    key pair change

--- main.tf
+++ main.tf
@@ -3,32 +3,8 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block       = "172.16.0.0/16"
+  cidr_block = "172.16.0.0/16"
   instance_tenancy = "default"
-
-  tags = {
-    Name = "main"
-  }
-}
-
-# Create an internet gateway and attach it to the VPC
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
-}
-
-# Create a route table and associate it with the VPC
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
   tags = {
     Name = "main"
   }
@@ -46,14 +22,14 @@ resource "aws_security_group" "jenkins-sg-2022" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
+ ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # outbound from Jenkins server
+ # outbound from jenkis server
   egress {
     from_port   = 0
     to_port     = 65535
@@ -67,47 +43,20 @@ resource "aws_security_group" "jenkins-sg-2022" {
 }
 
 resource "aws_instance" "myFirstInstance" {
-  ami                    = var.ami_id
-  key_name               = var.key_name
-  instance_type          = var.instance_type
+  ami = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
-
-  tags = {
+  tags= {
     Name = var.tag_name
   }
-
-  # Associate the instance with the public subnet
-  network_interface {
-    device_index = 0
-
-    subnet_id = aws_subnet.public.id
-
-    associate_public_ip_address = true
-  }
-}
-
-# Create a public subnet in the VPC
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "172.16.1.0/24"
-
-  tags = {
-    Name = "public"
-  }
-}
-
-# Associate the route table with the public subnet
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.main.id
 }
 
 # Create Elastic IP address
 resource "aws_eip" "myFirstInstance" {
   vpc = true
   instance = aws_instance.myFirstInstance.id
-
-  tags = {
+tags= {
     Name = "my_elastic_ip"
   }
 }
