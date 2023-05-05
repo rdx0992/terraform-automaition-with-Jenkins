b2b1d6af83941dca1596b4b38caa425476751ef4
unknown <errahul0992@gmail.com>
2023-05-05 19:57:36 +0530

    key pair change

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "jenkins-sg-2022" {
 }
 
 resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
+  ami = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
