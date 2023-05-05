e510696f0052887d3b8cd2f8868ec9350c377c0a
unknown <errahul0992@gmail.com>
2023-05-05 20:01:22 +0530

    key pair change

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+       default     = "us-east-1a" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+    default     =  "Webappdeploy" 
 }
 
 variable "instance_type" { 
